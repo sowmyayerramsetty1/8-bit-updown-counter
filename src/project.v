ded64cf257530462dbf21e9bf7716d4df7244a49
sowmyayerramsetty1 <yerramsetty.sowmya0@gmail.com>
2025-09-09 15:07:18 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,23 +5,39 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_sowmya_updown_counter (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       ena,      // always 1 when the design is powered (ignore)
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    // Internal synchronous reset signal (active high)
+    wire reset = ~rst_n;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire enable  = ui_in[0];
+    wire up_down = ui_in[1];
+
+    reg [7:0] q;
+
+    always @(posedge clk) begin
+        if (reset)
+            q <= 8'b00000000;
+        else if (enable) begin
+            if (up_down)
+                q <= q + 1;
+            else
+                q <= q - 1;
+        end
+    end
+
+    assign uo_out = q;      // Output the counter value on uo_out bus
+    assign uio_out = 8'b0;  // Force IO output bus to zero
+    assign uio_oe  = 8'b0;  // Disable all IO output drivers
 
 endmodule
+
